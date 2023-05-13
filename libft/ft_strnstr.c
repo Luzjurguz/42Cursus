eb6827a34d08e1c2a57e02f447172e1e3bb921b6
Luz Maria Jurado Guzman <luz-mjur@c1r2s5.42malaga.com>
2023-05-13 17:12:44 +0200

    subida

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,25 +6,38 @@
 /*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 18:40:15 by luz-mjur          #+#    #+#             */
-/*   Updated: 2023/05/13 14:23:21 by luz-mjur         ###   ########.fr       */
+/*   Updated: 2023/05/13 17:08:09 by luz-mjur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	cont;
-
+	size_t 	cont1;
+	char	*b;
+	char	*l;
+	char	c;
+	
 	cont = 0;
-	if (!little)
-		return ((char *)big);
-	while (cont < len)
+	cont1 = 0;
+	c = cont1;
+	b = (char *)big;
+	l = (char *)little;
+	if (!l)
+		return ((char *)b);
+	while (b [cont] != '\0')
 	{
-		if (little[cont] == big[cont])
-			return ((char *)big[cont]);
-		else
+		cont++;
+		if (l[cont] == b[cont1])
+		{
+			return ((char *)b + cont);
 			cont++;
+			cont1++;
+		}	
+		else if(l[cont]!= b[cont1])
+			cont1++;	
 	}
-	return (0);
+	return(0);
 }
