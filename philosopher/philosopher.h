f52026fad90d473e1a7322ab1d65a3d8c27ec502
Luz Maria Jurado Guzman <luz-mjur@c3r10s1.42malaga.com>
2024-11-25 15:23:25 +0100

    philo

---
+++ philosopher/philosopher.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 20:22:09 by luz-mjur          #+#    #+#             */
+/*   Updated: 2024/11/24 20:25:11 by luz-mjur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+#	define PHILOSOPHER_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+#endif
