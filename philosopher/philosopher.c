3fb836e20d82fb65b5aefcd5e136d9b6103f39d9
Luz Maria Jurado Guzman <luz-mjur@c3r10s1.42malaga.com>
2024-11-25 17:18:22 +0100

    philo1

--- philosopher/philosopher.c
+++ philosopher/philosopher.c
@@ -6,8 +6,42 @@
 /*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:11:54 by luz-mjur          #+#    #+#             */
-/*   Updated: 2024/11/24 20:21:40 by luz-mjur         ###   ########.fr       */
+/*   Updated: 2024/11/25 17:17:58 by luz-mjur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
  #include "philosopher.h"
+ 
+
+
+void	print_status(char c, int time, t_philo id)
+{
+	if(c == d)
+	{
+//		printf("%")		
+	}
+}
+int pars_arg(t_fork *fork, t_philo *philo, char **av)
+{
+	philo->number_of_philosophers = ft_atoi(av[1]);
+	fork->number_of_forks = ft_atoi(av[1]);
+	philo->time_to_die = ft_atoi(av[2]);
+	philo->time_to_eat = ft_atoi(av[3]);
+	philo->time_to_sleep = ft_atoi(av[4]);
+	if(av[5])
+	{
+		philo->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
+	} 
+}
+int main(int ac, char **av)
+{
+	t_philo *philo;
+	t_fork *fork;
+	
+	if(ac == 5 || ac == 6)
+	{
+		pars_arg(fork, philo, **av);	
+	}
+	else 
+			return(printf("%s" ,"ERROR : wrong arguments!")); 
+}
--- philosopher/philosopher.h
+++ philosopher/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:22:09 by luz-mjur          #+#    #+#             */
-/*   Updated: 2024/11/25 15:25:39 by luz-mjur         ###   ########.fr       */
+/*   Updated: 2024/11/25 16:43:29 by luz-mjur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,37 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef struct s_philo
+{
+	int id;
+	int	number_of_philosophers;
+	int	time_to_die;
+	int time_to_sleep;
+	int time_to_eat;
+	int number_of_times_each_philosopher_must_eat;
+	
+} t_philo;
 
-#endifgit c
+typedef struct s_fork
+{
+	int number_of_forks;
+	int r_fork;
+	int l_fork;
+	int f_mutex;
+	
+} t_fork;
+
+typedef struct s_data
+{
+	t_philo	*philos;
+	t_fork *forks;
+		
+} t_data;
+
+
+
+int			ft_atoi(const char *str);
+void	print_status(char c, int time, t_philo id);
+
+
+#endif
