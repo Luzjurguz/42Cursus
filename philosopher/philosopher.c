7e6dae2d7edc26f88344bd63f332c49d56158324
Luz Maria Jurado Guzman <luz-mjur@c3r8s3.42malaga.com>
2024-11-27 20:52:11 +0100

    philo2

--- philosopher/philosopher.c
+++ philosopher/philosopher.c
@@ -6,42 +6,73 @@
 /*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 19:11:54 by luz-mjur          #+#    #+#             */
-/*   Updated: 2024/11/25 17:17:58 by luz-mjur         ###   ########.fr       */
+/*   Updated: 2024/11/27 20:51:29 by luz-mjur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
- #include "philosopher.h"
- 
+#include "philosopher.h"
 
+int	create_philoarr(t_philo **philos, int n)
+{
+	int	a;
 
-void	print_status(char c, int time, t_philo id)
+	a = 1;
+	**philos = malloc(sizeof(t_philo) * n);
+	if (!**philos)
+		return (NULL);
+	while (a < n)
+	{
+		philos[a] = malloc(sizeof(t_philo));
+		if (!philos[a])
+			return (NULL);
+		philos[a]->id = a;
+		a++;
+	}
+}
+int	create_forkarr(t_fork *fork, int n)
+{
+	int	a;
+
+	a = 0;
+	fork = malloc(sizeof(t_fork) * n);
+	if (!fork)
+		return (NULL);
+}
+
+/*void	print_status(char c, int time, t_philo id)
 {
 	if(c == d)
 	{
-//		printf("%")		
+//		printf("%")
 	}
 }
-int pars_arg(t_fork *fork, t_philo *philo, char **av)
+*/
+int	pars_arg(t_fork *fork, t_philo *philo, char **av)
 {
 	philo->number_of_philosophers = ft_atoi(av[1]);
 	fork->number_of_forks = ft_atoi(av[1]);
 	philo->time_to_die = ft_atoi(av[2]);
 	philo->time_to_eat = ft_atoi(av[3]);
 	philo->time_to_sleep = ft_atoi(av[4]);
-	if(av[5])
+	if (av[5])
 	{
 		philo->number_of_times_each_philosopher_must_eat = ft_atoi(av[5]);
-	} 
+	}
 }
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_philo *philo;
 	t_fork *fork;
-	
-	if(ac == 5 || ac == 6)
+	t_data **philos;
+	if (ac == 5 || ac == 6)
 	{
-		pars_arg(fork, philo, **av);	
+		pars_arg(fork, philo, **av);
 	}
-	else 
-			return(printf("%s" ,"ERROR : wrong arguments!")); 
+	else
+		return (printf("%s", "ERROR : wrong arguments!"));
+
+	create_philoarr(**philos, philo->number_of_philosophers);
+	create_forkarr(fork, fork->number_of_forks);
+	
+	
 }
--- philosopher/philosopher.h
+++ philosopher/philosopher.h
@@ -6,48 +6,47 @@
 /*   By: luz-mjur <luz-mjur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:22:09 by luz-mjur          #+#    #+#             */
-/*   Updated: 2024/11/25 17:22:58 by luz-mjur         ###   ########.fr       */
+/*   Updated: 2024/11/27 20:50:20 by luz-mjur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHER_H
-#	define PHILOSOPHER_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+# define PHILOSOPHER_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_philo
 {
-	int id;
-	int	number_of_philosophers;
-	int	time_to_die;
-	int time_to_sleep;
-	int time_to_eat;
-	int number_of_times_each_philosopher_must_eat;
-	
-} t_philo;
+	int		id;
+	int		number_of_philosophers;
+	int		time_to_die;
+	int		time_to_sleep;
+	int		time_to_eat;
+	int		number_of_times_each_philosopher_must_eat;
+
+}			t_philo;
 
 typedef struct s_fork
 {
-	int number_of_forks;
-	int r_fork;
-	int l_fork;
-	int f_mutex;
-	
-} t_fork;
+	int		number_of_forks;
+	int		r_fork;
+	int		l_fork;
+	int		f_mutex;
+
+}			t_fork;
 
 typedef struct s_data
 {
-	t_philo	*philos;
-	t_fork *forks;
-		
-} t_data;
+	t_philo	**philos;
+	t_fork	*forks;
 
-
+}			t_data;
 
 int			ft_atoi(const char *str);
-void	print_status(char c, int time, t_philo id);
-
+void		print_status(char c, int time, t_philo id);
+int			create_philoarr(t_philo **philos, int n);
+int			create_forkarr(t_fork *fork, int n);
 
 #endif
